24f591e0d451922d62e258557c6093ce5bc9d1c3
Rob <robert890501@mail.ru>
2021-07-21 13:57:50 +0300

    praktica 1

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+'use stric';
+
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?',"");
+
+const personalMovieDB = {
+      count: numberOfFilms,
+      movies: {},
+      actors: {},
+      genres: [],
+      privat: false
+};
+
